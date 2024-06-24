3efa6989226f4ff5d88628c72705e9d55b651595
RitikLahari <158006573+RitikLahari@users.noreply.github.com>
2024-06-25 01:10:14 +0530

    Update Contact.jsx

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -73,32 +73,11 @@ const Contact = () => {
                     <img src={vg} alt='background' />
                 </aside>
             </div>
-            <footer>
-                <div className='footer'>
-                    <div className='wave'>
-                        <div className='wave' id='wave1'></div>
-                        <div className='wave' id='wave2'></div>
-                        <div className='wave' id='wave3'></div>
-                        <div className='wave' id='wave4'></div>
-                    </div>
-                    <div className='social-icon'>
-                        <ul>
-                            <li><a href='#'><i className="zmdi zmdi-github-alt"></i></a></li>
-                            <li><a href='#'><i className="zmdi zmdi-linkedin"></i></a></li>
-                            <li><a href='#'><i className="zmdi zmdi-instagram"></i></a></li>
-                            <li><a href='#'><i className="zmdi zmdi-twitter"></i></a></li>
-                            <li><a href='#'><i className="zmdi zmdi-google-maps"></i></a></li>
-                        </ul>
-                        <ul className='menu'>
-                            <li><a href='#'>Home</a></li>
-                            <li><a href='about'>About</a></li>
-                            <li><a href='contact'>Contact</a></li>
-                            <li><a href='register'>Signup</a></li>
-                            <li><a href='signin'>Login</a></li>
-                        </ul>
-                    </div>
-                </div>
-            </footer>
+            <footer className="footer">
+      <div className="footer-content">
+        <p>&copy; 2024 Your Company Name. All rights reserved.</p>
+      </div>
+    </footer>
         </>
     );
 };
